4e3750d595e0f0710ee7fc933b2f98fd145e0e63
Jessica Antunes <jessantunes@protonmail.com>
2021-03-09 12:36:33 -0500

    commit tree b5a183ed44fd9aca1946c0edd05ea3dfef8a4f57

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,74 @@
 class Owner
-  # code goes here
+  attr_accessor :cat, :dog
+  attr_reader :name, :species
+
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    return "I am a #{self.species}."
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def cats 
+    Cat.all.select { | cat | cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select { | dog | dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self) 
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+    
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    self.dogs.each do |dog| 
+      dog.mood = "nervous" 
+      dog.owner = nil
+    end
+    self.cats.each do |cat| 
+      cat.mood = "nervous" 
+      cat.owner = nil
+    end
+  end
+
+  def list_pets
+    many_dogs = self.dogs.count
+    many_cats = self.cats.count
+    return "I have #{many_dogs} dog(s), and #{many_cats} cat(s)."
+  end
+
+
+
+
 end
